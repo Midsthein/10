e0bfb7f51338fa6ca0fb94cfc19738b2b12bfc85
Midsthein <149871522+Midsthein@users.noreply.github.com>
2023-11-16 18:03:19 +0200

    Add files via upload

---
+++ 5/5/5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Област визначення функції
+bool area(double x) {
+    return (x >= -1 && x <= 1);
+}
+
+// Значення виразу
+double value(double x) {
+    return (x * x + 1);
+}
+
+int main() {
+    // Введення значення аргументу
+    double x;
+    cout << "x = ";
+    cin >> x;
+
+    // Перевірка області визначення
+    if (!area(x)) {
+        cout << "x не належить області визначення функції" << endl;
+        return 0;
+    }
+
+    // Знаходження значення виразу
+    double v = value(x);
+
+    // Виведення результату
+    cout << "Значення виразу: " << v << endl;
+
+    return 0;
+}
+
